5c3a2838d0acbc6ee4be0f900eb815406a739c82
callum <callum@thoughtmachine.net>
2024-11-20 10:45:14 +0000

    feat(nvim) pin black version

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -167,7 +167,7 @@ require('mason').setup()
 local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
   'stylua', -- Used to format lua code
-  'black',
+  { 'black', version = '23.7.0' },
   'goimports',
   'prettier',
 })
